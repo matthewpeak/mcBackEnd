5ee509c93b602cd744b13b3542a92e7b123ebe5b
matthewpeak <matthewpeak@gmail.com>
2021-09-16 19:46:57 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eeb037dc10094244c0f226d413359a08'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '964a94f4-2eda-433d-aea6-3f6ea0f3299e'
+};
